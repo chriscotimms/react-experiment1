cf2d07c0cda11c53995c690deca686aec1bad835
chriscotimms <chriscotimms@gmail.com>
2024-02-14 11:37:18 +0000

    test change

--- src/data/tasks.js
+++ src/data/tasks.js
@@ -1,8 +1,8 @@
 // tasks.js
 const tasks = [
-    { id: 1, text: 'Task 1', completed: false },
-    { id: 2, text: 'Task 2', completed: true },
-    { id: 3, text: 'Task 3', completed: false }
+    { id: 1, text: 'collect parcel', completed: false },
+    { id: 2, text: 'Do shopping', completed: true },
+    { id: 3, text: 'Complete git push', completed: false }
   ];
   
   export default tasks;
